6e77deb1cb324df28539bc62ce6ed76ef1f5c9db
jayaramsivaramannair <jayaramsivaramannair@gmail.com>
2021-05-16 09:03:42 -0400

    Implemented restrict middleware to allow only authorized users

--- api/middleware/restricted.js
+++ api/middleware/restricted.js
@@ -1,5 +1,9 @@
-module.exports = (req, res, next) => {
-  next();
+const { JWT_SECRET } = require("../secrets.js")
+const jwt = require("jsonwebtoken")
+const bcrypt = require("bcryptjs")
+
+module.exports = async (req, res, next) => {
+
   /*
     IMPLEMENT
 
@@ -11,4 +15,28 @@ module.exports = (req, res, next) => {
     3- On invalid or expired token in the Authorization header,
       the response body should include a string exactly as follows: "token invalid".
   */
+  try {
+    const token = req.headers.authorization
+
+    //Checks if the token is provided or not
+    if (!token) {
+      return res.status(401).json({
+        message: "token required",
+      })
+    }
+
+    jwt.verify(token, JWT_SECRET, (err, decoded) => {
+      if (err) {
+        return res.status(401).json({
+          message: "token invalid",
+        })
+      }
+
+      req.token = decoded //This token will be accessible by middlewares downstream
+      next();
+    })
+
+  } catch (err) {
+    next(err)
+  }
 };
